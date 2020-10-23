b6add4af534a7cfaeb56af40031ddda2f17c05cc
matthias <matthias@localhost.localdomain>
2020-10-23 15:06:09 +0200

    Begin templating home

--- src/App.vue
+++ src/App.vue
@@ -62,14 +62,12 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 footer {
   position: absolute;
   bottom: 0;
-  width: 100%;
 }
-@import "/public/css/index.css"
+</style>
+<style lang="scss">
+@import './public/css/style.css';
 </style>
