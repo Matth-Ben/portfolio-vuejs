4d7a1a197951a134217d0fdb9664eda4d58d657f
Matth-Ben <m.benoit0702@gmail.com>
2020-11-04 00:09:50 +0100

    Init part contact & css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,9 @@
     </header>
     <main>
       <article id="projects">
+        <div class="projects-bloc-title">
+          <h6>Mes Projets</h6>
+        </div>
         <div class="content">
           <ul class="list-project">
             <li class="item-project" v-for="project in projects">
@@ -24,6 +27,9 @@
         </div>
       </article>
       <article id="about-me">
+        <div class="about-bloc-title">
+          <h6>A propos de moi</h6>
+        </div>
         <div class="content">
           <div class="content-about d-flex align-items-center">
             <div>
@@ -40,6 +46,38 @@
           </div>
           <div class="img-bg">
             <img src="../public/images/test-img-about.jpg" />
+          </div>
+        </div>
+      </article>
+      <article id="contact">
+        <div class="contact-bloc-title">
+          <h6>Contact</h6>
+        </div>
+        <div class="content">
+          <div class="contact-content">
+            <p>I'm most active and reachable <a href="mailto:m.benoit0702@gmail.com">via email</a>.</p>
+            <div class="row">
+              <div class="col-12 col-md-6 contact-content-site">
+                <div class="contact-content-site-block">
+                  <h4 class="link-underline"><a href=""><i class="fab fa-twitter"></i>Twitter</a></h4>
+                  <p>Home of the majority of my social sharing about new projects progress, new tech, and killer puns.</p>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 contact-content-site">
+                <div class="contact-content-site-block">
+                  <h4 class="link-underline"><a href=""><i class="fab fa-github"></i>Github</a></h4>
+                  <p>
+                    Open-source contributions and shared projects. Most of my personal projects are public repos here!
+                  </p>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 contact-content-site">
+                <div class="contact-content-site-block">
+                  <h4 class="link-underline"><a href=""><i class="fab fa-linkedin-in"></i>Linkedin</a></h4>
+                  <p>For demos of new tech explorations, animation, and interaction.</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </article>
@@ -62,27 +100,26 @@ export default {
   },
   mounted: function () {
     let self = this
-    async function getProjects() {
-      try{
+    async function getProjects () {
+      try {
         const response = await ProjectsService.getProjects()
         console.log(response)
         self.airtableResponse = response.data.records
-
-      }catch(err){
+      } catch (err) {
         console.log(err)
       }
     }
     getProjects()
   },
   computed: {
-    projects(){
+    projects () {
       let self = this
       let projectList = []
       for (var i = 0; i < self.airtableResponse.length; i++) {
-        if (self.airtableResponse[i].fields.Published){
+        if (self.airtableResponse[i].fields.Published) {
           let project = {
             title: self.airtableResponse[i].fields.Title,
-            date: self.airtableResponse[i].fields["Date Published"],
+            date: self.airtableResponse[i].fields['Date Published'],
             snippet: self.airtableResponse[i].fields.Excerpt,
             image: self.airtableResponse[i].fields.Image[0].url,
             slug: self.airtableResponse[i].fields.slug
